40a64305ae1a3497fe2786736e4c64de1b7f6d20
naffiziqbal <f.nafiziqbal@gmail.com>
2022-09-22 00:42:11 +0600

    Adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  return (
+    <div className="all-country-container">
+      <h3>All Countires</h3>
+      <div className="country-container">
+        {
+          countries.map(country => <Country
+
+            country = { country}
+            key = { country.cca3}
+            // name={country.name}
+            // capital={country.capital}
+            // area={country.area}
+            // flags={country.flags.svg}
+
+          />)
+        }
+      </div>
+
+    </div>
+  )
+}
+
+function Country(props) {
+  const {name, flags, area, capital,} = props.country
+  return (
+    <div className="item">
+      <div className="info">
+        <h2>Name:- {name?.common ? name.common : " N/A"}</h2>
+        <h4>Official Name:- {name?.official ? name.official : " N/A"}</h4>
+        <p>Capital:- {capital ? capital : " N/A"}</p>
+        {/* <p>Language:- {languages ? languages : " N/A"}</p> */}
+        <p>Area : {area ? area : 'N/A'}</p>
+      </div>
+      <img src={flags.svg}  alt= " Flag"/>
+    </div>
+  )
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LoadCountries />
     </div>
   );
 }
